4d1191a4734b46a08b3132eb4737542258fb86db
ANUDT <raswarup@gmail.com>
2024-09-22 16:56:49 +0100

    changes syntax to debug

--- Program.cs
+++ Program.cs
@@ -5,17 +5,19 @@ namespace MyApp
 {
     internal class Program
     {
-        //Main method, place to put the code inside "entry method", code will start here
+
         static void Main(string[] args)
         {
-
-            //class is a container with a bunch of things inside console. will bring options 
-            //with a Wrench = variable or property with this we can change our console (color, size or title)
-            //box = are for method or function these are comments to ask the console to do different things maybe to write a line of text or user to type something in
             Console.Title = "Sharpy";
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WindowHeight = 40;
 
-            Console.WriteLine("Hello World!");
-            //waiting for any type of input from your keyboard and will then close when pressing any key] 
+            Console.WriteLine("Hello what is up?");
+            Console.ReadLine();
+
+            Console.WriteLine("My name is Sharp the Shark, I am an AI figure and will look after you today?\nWhat can you do?");
+            Console.WriteLine("I can sing a song for you");
+
             Console.ReadKey();
         }
     }
