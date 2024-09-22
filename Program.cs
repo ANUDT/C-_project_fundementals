74294c3aa72a0f18af6b41247a3b81e859766afb
ANUDT <raswarup@gmail.com>
2024-09-22 15:54:46 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //waiting for any type of input from your keyboard and will then close when pressing any key] 
+            Console.ReadKey ();
+        }
+    }
+}
+
